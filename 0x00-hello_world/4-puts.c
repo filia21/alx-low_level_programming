a56f4d1d383196eea5fa962dc09d035d486d3d29
filia21 <e.alemayehu.ea@gmail.com>
2022-06-02 06:51:17 -0700

    ss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main function with 
+ * zero return value
+ */
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle\n");
 			return (0);
 }
