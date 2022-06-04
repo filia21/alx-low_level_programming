34437fb6cb206016433601d857770dd9278ab21b
filia21 <e.alemayehu.ea@gmail.com>
2022-06-03 23:12:58 -0700

    w

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+/**
+ * main - entry function
+ * Return : Always 0 (sucess)
+ */
+int main(void)
+{
+	int d;
+	
+	for (d = 0; d <= 9; d++)
+
+		putchar(d);
+	
+	putchar('\n');
+
+	return 0;
+
+}
+
+
