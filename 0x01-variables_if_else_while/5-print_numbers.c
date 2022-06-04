ef238e0a7cc8ff10d28d2842cb77dd69c94f2ec0
filia21 <e.alemayehu.ea@gmail.com>
2022-06-03 22:48:32 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+/** main - entry function
+ * Return : 0 always
+ * */
+int main (void)
+{
+
+	int f;
+
+	for (f=0; f<10; f++)
+
+		printf("%d" , f);
+	printf('\n);
+
+	return (0);
+
+}
