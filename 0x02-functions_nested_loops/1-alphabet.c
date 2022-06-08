15c3f6fba638d107ed68d65675e9336909733d8b
filia21 <e.alemayehu.ea@gmail.com>
2022-06-07 23:01:59 -0700

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include<unistd.h>
+#include "main.h">
+/**
+* main - Entry point
+* Return: Always 0 (Success
+*/
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+
+}
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+
+	char i;
+
+	for (i = 'a';i <= 'z';i++)
+		_putchar(i);
+	_putchar('\n');
+
+	return (0);
+
+}
+
