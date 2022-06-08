0cf184711ff71bf4ddfd14c2f776d9dc82edf321
filia21 <e.alemayehu.ea@gmail.com>
2022-06-07 23:52:20 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	char alp;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+
+			_putchar(alp);
+
+		_putchar('\n');
+
+	}
+
+}
