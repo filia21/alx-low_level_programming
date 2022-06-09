56ba3af76d2163544ea106be38d104f78e2dfdd9
filia21 <e.alemayehu.ea@gmail.com>
2022-06-09 04:26:24 -0700

    c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,18 +5,20 @@
  */
 int main(void)
 {
+	
 	int i;
 
 	printf("infinte loop coming :(\n");
 
 	i = 0;
 
-	/*while (i < 0)*/
-	/*{*/
+	/* while (i < 0)*/
+	/* {*/
 	/*	putchar(i);*/
-	/*}*/
+	/* }*/
 
 	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
+
 }
