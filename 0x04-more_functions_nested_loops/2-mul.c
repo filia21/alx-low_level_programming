3c235f73426f2845931e31940dfd1d1ba7d13902
filia21 <e.alemayehu.ea@gmail.com>
2022-06-09 14:04:36 -0700

    d

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ * mul - functions that multiply two intigers
+ * @a: first variable
+ * @b: second variable
+ * Return: int.
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+}
