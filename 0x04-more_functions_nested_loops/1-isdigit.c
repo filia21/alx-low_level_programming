e48168ba89038a38c68ac5c79b5212a4ea482784
filia21 <e.alemayehu.ea@gmail.com>
2022-06-09 13:51:44 -0700

    l

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - identifies the digit
+ * @c: the input value
+ * Return: int.
+ */
+
+int _isdigit(int c)
+{
+	if(c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
