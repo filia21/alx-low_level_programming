aeacae05d9a2dae667256ccd0ec40f5c79d3bd0b
filia21 <e.alemayehu.ea@gmail.com>
2022-06-09 04:00:51 -0700

    f

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - causes infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinte loop coming :(\n");
+
+	i = 0;
+
+	/*while (i < 0)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
