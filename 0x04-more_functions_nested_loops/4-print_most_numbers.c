9a41bba8b2233093f1e574be76d75afd75164cc9
filia21 <e.alemayehu.ea@gmail.com>
2022-06-10 00:08:07 -0700

    d

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number form 0 to 9 except 2 & 4
+ * Return: Nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i ='0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+
+	}
+	_putchar('\n');
+
+}
