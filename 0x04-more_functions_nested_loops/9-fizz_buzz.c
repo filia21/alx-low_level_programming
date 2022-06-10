e917c8d424272cfe1ec84be403f612bd75c57866
filia21 <e.alemayehu.ea@gmail.com>
2022-06-10 05:51:30 -0700

    q

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,19 @@ int main(void)
 
 	for (n = 1; n < 100; n++)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
 		else if (n % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (n % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
 			printf("%d ", n);
-	
-		printf("\n");
+
+		if (n < 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
 
 	return (0);
