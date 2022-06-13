0c4b973a710cd9f830555fd34d0b11694e39f616
filia21 <e.alemayehu.ea@gmail.com>
2022-06-13 09:04:12 -0700

    f

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints string
+ * @str: string to be printed
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
