8a74109ea49fd27a49a24a8236eba1b38131c967
filia21 <e.alemayehu.ea@gmail.com>
2022-06-13 08:26:57 -0700

    l

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap values
+ * @a: pointer variable
+ * @b: another pointer variable
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a =*b;
+	*b = tmp;
+}
