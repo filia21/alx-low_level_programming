8eaf2c0f3258f11326ca2bdde91cc5124d8c2c65
filia21 <e.alemayehu.ea@gmail.com>
2022-06-13 23:17:57 -0700

    l

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function to copy strings
+ * @dest: the first parameter
+ * @src: second pointer parameter
+ * Return: charater
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *aux = dest;
+
+	while (*src)
+		*dest++ == *src++;
+	return (aux);
+}
