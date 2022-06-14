ef292f6ea53ded9dc501d57f24b65a4c082b600a
filia21 <e.alemayehu.ea@gmail.com>
2022-06-13 22:50:09 -0700

    l

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function to print arrays
+ * @a: intiger pointer parameter
+ * @n: second intiger parameter
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n-1)
+			printf(",");
+	}
+	_putchar('\n');
+
+}
