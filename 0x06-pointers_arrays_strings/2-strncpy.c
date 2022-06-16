c882b594d847cb66905e346f9c21d4a3cf7b5524
filia21 <e.alemayehu.ea@gmail.com>
2022-06-16 05:14:33 -0700

    f

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies string
+ * @dest: first pointer 
+ * @src: second pointer
+ * @n: third parameter
+ * Return: character
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0, i = 0;
+	char *tmp = dest, *start = src;
+
+	while(*src)
+	{
+		len++;
+		src++;
+	}
+	len++;
+
+	if (n > len)
+		n = len;
+
+	src = start;
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (tmp);
+}
