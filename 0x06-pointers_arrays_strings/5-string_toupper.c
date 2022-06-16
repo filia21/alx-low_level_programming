4709d5a6139f68cb34858b754f4134ca397ef273
filia21 <e.alemayehu.ea@gmail.com>
2022-06-16 06:42:24 -0700

    t

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all letter to uppercase
+ * @s: first parameter
+ * @Return: intinger
+ */
+char *string_toupper(char *s)
+{
+	char *start = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s -= 32;
+		s++;
+	}
+
+	return (start);
+}
