65a2dc011ca1abfcc2e429dc5eb1c45b31f7d29a
filia21 <e.alemayehu.ea@gmail.com>
2022-06-23 06:04:33 -0700

    d

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _sqrt_recursion - perform squre root
+ * @n: first parameter
+ * Return: intigers
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (0);
+	return (_sqrt(1, n));
+}
+
+/**
+ * _sqrt - perform artimetic fucyion
+ * @i: first parameter
+ * @j: second parameter
+ * Return: intiger
+ */
+int _sqrt(int i, int j)
+{
+	if (i > j)
+		return (-1);
+	else if (i * i == j)
+		return (i);
+	return (_sqrt(i + 1, j));
+
+}
