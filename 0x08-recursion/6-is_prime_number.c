a8deb9cad1e6fe647d9c9f19862e699fa302c191
filia21 <e.alemayehu.ea@gmail.com>
2022-06-23 06:38:12 -0700

    s

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,7 +13,7 @@ int is_prime_number(int n)
 		return (0);
 	if (n < 3)
 		return (1);
-	return (is_divisable(n, civ));
+	return (is_divisable(n, div));
 }
 /**
  * is_divisable - checks if number is prime or not
