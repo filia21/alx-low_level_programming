9de601897e4b3927fc34e685a3b9622e28fab970
filia21 <e.alemayehu.ea@gmail.com>
2022-06-29 01:36:45 -0700

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,20 +8,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int i, j, k, prod;
+
+	prod = 0;
 
 	if (argc != 3)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(agrv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-	}
 
+	else if (argc == 3)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (i == 1)
+				j = atoi(argv[i]);
+			else if (i == 2)
+				k = atoi(argv[i]);
+		}
+		prod = j * k;
+		printf("%d\n", prod);
+	}
 	return (0);
 }
