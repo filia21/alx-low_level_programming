66d56f3c40d0f85eed8d0342c4e0eb19e8e140e6
filia21 <e.alemayehu.ea@gmail.com>
2022-06-28 22:36:46 -0700

    s

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print out all arguments
+ * @argc: argument counter
+ * @argv: array parameter
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
