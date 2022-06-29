2f3cedfff9693bfcc50d34e19f66cbff9b1763b0
filia21 <e.alemayehu.ea@gmail.com>
2022-06-28 22:56:27 -0700

    s

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print reuslt of multiplication
+ * @argc: count of parameter
+ * @argv: array parameter
+ * Retun: intiger value
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (0);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+
+		printf("%d\n", result);
+		return (0);
+	}
+}
