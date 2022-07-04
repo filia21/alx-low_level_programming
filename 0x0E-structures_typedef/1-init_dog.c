94930fed1bf55f87723c2906f8fa82c3d0d30c03
filia21 <e.alemayehu.ea@gmail.com>
2022-07-04 06:30:28 -0700

    r

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,12 +7,12 @@
  * @name: second parameter
  * @age: second parameter
  * @owner: third parameter
- * Return - Nothing
+ * Return: Nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return;
+		return (0);
 
 	d->name = name;
 	d->age = age;
