97bdca2778ff47b701c4e5b1898f340ad232f482
filia21 <e.alemayehu.ea@gmail.com>
2022-07-04 07:02:37 -0700

    t

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib>
+/**
+ * print_dog - print dog name age owner
+ * @d: first parameter
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}	
