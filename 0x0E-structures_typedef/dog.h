c0874b91459683589db6cba04b5dc682bf1fd28d
filia21 <e.alemayehu.ea@gmail.com>
2022-07-04 05:42:28 -0700

    f

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - short description
+ * @name: first type
+ * @age: second type
+ * @owner: third type
+ * Description: Longer description
+ */
+struct dog 
+{
+	char *name
+	float age
+	char *owner
+};
+#endif
