6ccd5fafb3253b0c71d5a0c3698a24dffeda7bee
filia21 <e.alemayehu.ea@gmail.com>
2022-07-04 06:24:13 -0700

    r

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - initialize struct
+ * @d: firist parameter
+ * @name: second parameter
+ * @age: second parameter
+ * @owner: third parameter
+ * Return - Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
