15682dd6e7a02dbd781af09cd4d2351845626fb9
filia21 <e.alemayehu.ea@gmail.com>
2022-07-05 03:56:32 -0700

    f

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - function to create new dog
+ * @name: first parameter
+ * @age: second parameter
+ * @owner: third parameter
+ * Return: Pointer
+ */
+dog_t *new_dog(char *name, char *owner, float age)
+{
+	dog_t p;
+
+	p = malloc(sizeof(dog_t));
+	
+	if (p == NULL)
+		return (NULL);
+	if (name == NULL)
+	{
+		free (p);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free (p);
+		free (name);
+		return (NULL);
+	}
+
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}
