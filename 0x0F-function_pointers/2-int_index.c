d70fe0357000053b2a10138e08b700ce0f732afd
filia21 <e.alemayehu.ea@gmail.com>
2022-07-08 01:51:51 -0700

    l

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (cmp == NULL || array == NULL)
 		return (-1);
+
 	if (size <= 0)
 		return (-1);
+
 	for (; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
+
 	if (i == size)
 		return (-1);
+
 	return (-1);
 }
