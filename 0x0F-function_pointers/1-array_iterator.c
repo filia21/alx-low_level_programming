fda83cb6da6f5b84f2b3c914f6ffed663af398e3
filia21 <e.alemayehu.ea@gmail.com>
2022-07-08 00:56:59 -0700

    y

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+
 /**
  * array_iterator - function to iterat elements
  * @array: first parameter
@@ -13,6 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
+
 	for(; i < size; i++)
 		action(array[i]);
 }
