9fece405fcc5ce57050ed3f9713112ffb6a85d32
filia21 <e.alemayehu.ea@gmail.com>
2022-07-14 06:16:08 -0700

    j

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - return number of linked list
+ * @h: first parameter
+ * Return: size_t
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int n = 0;
+
+	while(h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
