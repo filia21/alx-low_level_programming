572177b65c399e9815104fc593cbbe13c2d876cc
filia21 <e.alemayehu.ea@gmail.com>
2022-07-14 06:34:59 -0700

    s

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - function that frees
+ * @head: first parameter
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+	temp = head;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
