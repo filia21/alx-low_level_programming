7d16cb6855869c47316cd264e3fe0113f3a60729
filia21 <e.alemayehu.ea@gmail.com>
2022-07-14 06:24:40 -0700

    l

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - function that adds node
+ * @head: first parameter
+ * @str: second parameter
+ * Return: intiger
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	int length = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	while (str[length])
+		length++;
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
